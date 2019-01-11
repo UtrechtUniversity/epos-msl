7bb8cfd3ece7f831d79d44840fb9520fcd82c017
Lazlo Westerhof <l.r.westerhof@uu.nl>
2019-01-11 12:58:37 +0100

    Return error count as integer to Zabbix.

--- roles/zabbix-ckan/files/dailyFetchLogErrors.sh
+++ roles/zabbix-ckan/files/dailyFetchLogErrors.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-# \file dailyFetchLogErrors.sh
+# \file         dailyFetchLogErrors.sh
 # \brief        Counts the number of errors PER DAY in CKAN harvester fetch_consumer.log and present.
-# \copyright    Copyright (c) 2018, Utrecht University. All rights reserved.
-
+# \copyright    Copyright (c) 2019, Utrecht University. All rights reserved.
 
 #counts lines containing ERROR per day
-sudo -u ckan grep "ERROR" /var/log/ckan/std/fetch_consumer.log | cut -c 1-10 | uniq -c
+sudo -u ckan grep "$(date +"%F")" /var/log/ckan/std/fetch_consumer.log | grep -c "ERROR"
--- roles/zabbix-ckan/files/dailyGatherLogErrors.sh
+++ roles/zabbix-ckan/files/dailyGatherLogErrors.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # \file         dailyGatherLogErrors.sh
 # \brief        Counts the number of errors PER DAY in CKAN harvester gather_consumer.log and present.
-# \copyright    Copyright (c) 2018, Utrecht University. All rights reserved.
-
+# \copyright    Copyright (c) 2019, Utrecht University. All rights reserved.
 
 #counts lines containing ERROR per day
-sudo -u ckan grep "ERROR" /var/log/ckan/std/gather_consumer.log | cut -c 1-10 | uniq -c
+sudo -u ckan grep "$(date +"%F")" /var/log/ckan/std/gather_consumer.log | grep -c "ERROR"
