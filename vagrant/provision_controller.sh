9c39ad2913479bd859c0f0da660a3cb3c2c96b05
Sietse Snel <s.t.snel@uu.nl>
2024-03-25 16:50:43 +0100

    Controller pr. script: adapt for Ubuntu

--- vagrant/provision_controller.sh
+++ vagrant/provision_controller.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 # copyright Utrecht University
 
+set -e
+set -x
+
+sudo apt update
+
 # Install Git if not present.
 if ! command -v git >/dev/null; then
     echo "Installing Git."
-    sudo yum install git -y
+    sudo apt install git -y
 fi
 
 # Install Ansible if not present.
 if ! command -v ansible >/dev/null; then
     echo "Installing Ansible."
-    sudo yum install epel-release -y
-    sudo yum install ansible -y
+    sudo apt install ansible python3-pip -y
+    sudo pip3 install --upgrade ansible==6.7.0
 fi
 
 # Remove current version.
@@ -19,6 +24,8 @@ rm -rf ~/epos-msl
 
 # Clone epos-msl repository.
 git clone https://github.com/UtrechtUniversity/epos-msl.git
+cd epos-msl
+git checkout development
 
 # Set file permissions on SSH key to 0600.
 chmod 0600 ~/epos-msl/vagrant/ssh/vagrant
