3e5fb794665a58b98cccc544ca51d56318eda608
Sietse Snel <s.t.snel@uu.nl>
2025-01-22 23:18:26 +0100

    Update Ansible playbook for Docker Setup
    Will update README / documentation separately.

--- roles/zabbix_ckan/files/dailyErrorLog.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# \file         dailyFetchLogErrors.sh
-# \brief        Counts the number of errors PER DAY in CKAN uwsgi logs
-# \copyright    Copyright (c) 2019, Utrecht University. All rights reserved.
-
-#counts lines containing ERROR per day
-sudo -u ckan grep "$(date +"%F")" /var/log/ckan/ckan-uwsgi.stderr.log | grep -c "ERROR"
