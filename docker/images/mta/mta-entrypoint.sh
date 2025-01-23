1df189f6eeafda7de7f0c3ace428152b927ab496
Sietse Snel <s.t.snel@uu.nl>
2025-01-23 13:55:38 +0100

    Docker setup: add Postfix workaround DNS
    This ensures that name resolution settings are available in
    chrooted Postfix services.

--- docker/images/mta/mta-entrypoint.sh
+++ docker/images/mta/mta-entrypoint.sh
@@ -3,10 +3,14 @@
 if [ "$MTA_ROLE" == "mailpit" ]
 then BINDIP=$(ip addr | grep eth0 | grep inet | awk '{print $2}' | cut -d '/' -f 1)
      /mailpit.bin -s $BINDIP:25 --webroot mailpit
+
 elif [ "$MTA_ROLE" == "postfix" ]
 then echo "[$POSTFIX_RELAYHOST_FQDN]:$POSTFIX_RELAYHOST_PORT $POSTFIX_RELAYHOST_USERNAME:$POSTFIX_RELAYHOST_PASSWORD" > /etc/postfix/sasl_passwd
      cd /etc/postfix
      postmap sasl_passwd
+
+     # Needed to make name resolution work for Postfix
+     cp /etc/resolv.conf /var/spool/postfix/etc/resolv.conf
 
      # Update configuration values
 
