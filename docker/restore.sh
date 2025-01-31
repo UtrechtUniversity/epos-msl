de97ea553e08e0b39bdff49ed7083dabc0d49851
Sietse Snel <s.t.snel@uu.nl>
2025-01-31 14:44:38 +0100

    Restore script: restart application after restore
    As a precaution to avoid problems with stale connections, cached
    data, etc.

--- docker/restore.sh
+++ docker/restore.sh
@@ -37,3 +37,6 @@ echo "Reloading CKAN web server after database change ..."
 docker restart ckan
 echo "Rebuilding CKAN search index"
 docker exec -it ckan /bin/bash -c "/usr/lib/ckan/default/bin/ckan -c /etc/ckan/default/ckan.ini search-index rebuild"
+echo "Restarting the application ..."
+docker compose restart
+echo "Done."
