ad27fb5f6d911a301a5b9a64518dc135d1638ecd
Sietse Snel <s.t.snel@uu.nl>
2026-09-23 22:30:55 +0200

    Docker setup: exclude app dir for backup/restore
    Do not back up or restore /var/www/msl_api/storage/app
    dir. It needs to have the version from the image, because
    it's part of the MSL-API software.

--- docker/backup.sh
+++ docker/backup.sh
@@ -13,7 +13,7 @@ then echo "No staging dir provided. Setting it to current working directory."
      STAGINGDIR="."
 fi
 
-docker exec mslapi_web /bin/bash -c "cd /var/www/msl_api && tar cv .env storage" | gzip -9 > "${STAGINGDIR}/msl-api-data.tar.gz"
+docker exec mslapi_web /bin/bash -c "cd /var/www/msl_api && tar cv --anchored --exclude storage/app .env storage" | gzip -9 > "${STAGINGDIR}/msl-api-data.tar.gz"
 docker exec mslapi_web /bin/bash -c "mysqldump -h mslapi_db -u root "-p$MYSQL_ROOT_PASSWORD" mslapi" | gzip -9 > "${STAGINGDIR}/msl-api-db.sql.gz"
 docker exec ckan /bin/bash -c "cd /etc/ckan; tar cv default" | gzip -9 > "${STAGINGDIR}/ckan-settings.tar.gz"
 docker exec ckan /bin/bash -c "PGPASSWORD=\"\$POSTGRES_PASSWORD\" pg_dump -d ckan_default -h db -U ckan" | gzip -9 > "${STAGINGDIR}/ckan-db.sql.gz"
